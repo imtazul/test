e521b5cc1f5e7e33978c93c2b84769d9b5c98fb4
imtazul <imtazulhoque007@gmail.com>
2020-11-21 01:15:39 +0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For learning purpose</title>
+</head>
+<body>
+    
+
+    <h2>Amr vai tmr vai imtazul vai</h2>
+</body>
+</html>
